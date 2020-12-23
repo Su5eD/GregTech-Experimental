9d582e8c5f04dd6f4d03928c53b06811af038fc2
Su5eD <adamko.zachar@gmail.com>
2020-12-23 10:40:19 +0100

    Move the ItemTooltipEvent subscriber to ClientEventHandler

--- src/main/java/mods/gregtechmod/core/ClientEventHandler.java
+++ src/main/java/mods/gregtechmod/core/ClientEventHandler.java
@@ -1,10 +1,15 @@
 package mods.gregtechmod.core;
 
+import ic2.api.item.IC2Items;
 import mods.gregtechmod.api.util.GtUtil;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.event.RenderPlayerEvent;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
+
+import java.util.List;
 
 @Mod.EventBusSubscriber(value = Side.CLIENT)
 public class ClientEventHandler {
@@ -12,5 +17,24 @@ public class ClientEventHandler {
     @SubscribeEvent
     public static void onRenderPlayer(RenderPlayerEvent.Pre event) {
         if (GtUtil.getFullInvisibility(event.getEntityPlayer())) event.setCanceled(true);
+    }
+
+    @SubscribeEvent
+    public void onItemTooltip(ItemTooltipEvent event) {
+        ItemStack stack = event.getItemStack();
+        List<String> tooltip = event.getToolTip();
+
+        if (stack.isItemEqual(IC2Items.getItem("dust", "diamond"))) tooltip.add("C128");
+        else if (stack.isItemEqual(IC2Items.getItem("dust", "lead"))) tooltip.add("Pb");
+        else if (stack.isItemEqual(IC2Items.getItem("dust", "obsidian"))) tooltip.add("MgFeSi2O8");
+        else if (stack.isItemEqual(IC2Items.getItem("dust", "coal"))) tooltip.add("C2");
+        else if (stack.isItemEqual(IC2Items.getItem("dust", "iron"))) tooltip.add("Fe");
+        else if (stack.isItemEqual(IC2Items.getItem("dust", "gold"))) tooltip.add("Au");
+        else if (stack.isItemEqual(IC2Items.getItem("dust", "copper"))) tooltip.add("Cu");
+        else if (stack.isItemEqual(IC2Items.getItem("dust", "tin"))) tooltip.add("Sn");
+        else if (stack.isItemEqual(IC2Items.getItem("dust", "bronze"))) tooltip.add("SnCu3");
+        else if (stack.isItemEqual(IC2Items.getItem("dust", "silver"))) tooltip.add("Ag");
+        else if (stack.isItemEqual(IC2Items.getItem("dust", "clay"))) tooltip.add("Na2LiAl2Si2");
+        else if (stack.isItemEqual(IC2Items.getItem("misc_resource", "ashes"))) tooltip.add("C");
     }
 }
