9a5a18f249ffcf33a0d95d093855d06600002fd9
Su5eD <adamko.zachar@gmail.com>
2021-06-14 20:37:05 +0200

    Add FancyGradle maven repo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,6 +12,10 @@ pluginManagement {
         maven {
             url = uri("https://maven.minecraftforge.net/")
         }
+        maven {
+            name = "FancyGradle"
+            url = uri("https://gitlab.com/api/v4/projects/26758973/packages/maven")
+        }
     }
 }
 
