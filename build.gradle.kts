1a3bf3cd0252ccb0d3f320990d513910daa7e2f2
Su5eD <su5ed@outlook.com>
2021-09-26 14:29:33 +0200

    Cleanup buildscript

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import fr.brouillard.oss.jgitver.GitVersionCalculator
 import fr.brouillard.oss.jgitver.Strategies
 import net.minecraftforge.gradle.common.util.RunConfig
-import wtf.gofancy.fancygradle.patch.Patch
 import wtf.gofancy.fancygradle.script.extensions.curse
 import wtf.gofancy.fancygradle.script.extensions.deobf
 import java.time.LocalDateTime
@@ -17,9 +16,9 @@ plugins {
     `java-library`
     `maven-publish`
     idea
-    id("net.minecraftforge.gradle") version "5.0.11"
-    id("com.github.johnrengelman.shadow") version "6.1.0"
-    id("wtf.gofancy.fancygradle") version "1.0.0"
+    id("net.minecraftforge.gradle") version "5.1.+"
+    id("com.github.johnrengelman.shadow") version "7.0.0"
+    id("wtf.gofancy.fancygradle") version "1.1.+"
 }
 
 val versionMc: String by project
@@ -39,8 +38,8 @@ val versionEnergyControl: String by project
 val versionRailcraft: String by project
 val versionThaumcraft: String by project
 
-version = getGitVersion()
 group = "mods.su5ed"
+version = getGitVersion()
 setProperty("archivesBaseName", "gregtechmod")
 
 minecraft {
@@ -50,12 +49,13 @@ minecraft {
         val config = Action<RunConfig> {
             properties(
                 mapOf(
-                    "forge.logging.markers" to "SCAN,REGISTRIES,REGISTRYDUMP,COREMODLOG",
+                    "forge.logging.markers" to "SCAN,REGISTRIES",
                     "forge.logging.console.level" to "debug"
                 )
             )
             workingDirectory = project.file("run").canonicalPath
-            source(sourceSets["main"])
+            source(sourceSets.main.get())
+            forceExit = false
         }
 
         create("client", config)
@@ -65,60 +65,61 @@ minecraft {
 
 idea.module.inheritOutputDirs = true
 
-java.toolchain { 
-    languageVersion.set(JavaLanguageVersion.of(8))
-}
+java.toolchain.languageVersion.set(JavaLanguageVersion.of(8))
 
 fancyGradle {
     patches {
-        patch(Patch.RESOURCES, Patch.COREMODS, Patch.CODE_CHICKEN_LIB, Patch.ASM)
+        resources
+        coremods
+        codeChickenLib
+        asm
     }
 }
+
+val api: SourceSet by sourceSets.creating
 
 sourceSets {
-    create("api")
     main {
-        val output = sourceSets.getByName("api").output
-        compileClasspath += output
-        runtimeClasspath += output
+        compileClasspath += api.output
+        runtimeClasspath += api.output
     }
 }
+
+val shade: Configuration by configurations.creating
 
 configurations {
-    val shade = register("shade").get()
-    val impl = implementation.get()
-
-    impl.extendsFrom(shade)
-    getByName("apiImplementation").extendsFrom(impl)
+    implementation {
+        extendsFrom(shade)
+        
+        getByName("apiImplementation").extendsFrom(this)
+    }
 }
 
+val manifestAttributes = mapOf(
+    "Specification-Title" to "gregtechmod",
+    "Specification-Vendor" to "Su5eD",
+    "Specification-Version" to "1",
+    "Implementation-Title" to project.name,
+    "Implementation-Version" to project.version,
+    "Implementation-Vendor" to "Su5eD",
+    "Implementation-Timestamp" to LocalDateTime.now()
+)
+
 tasks {
-    build {
-        dependsOn("shadowJar", "devJar", "apiJar")
-    }
-
     jar {
-        from(sourceSets.getByName("api").output)
+        from(api.output)
 
-        manifest {
-            attributes(
-                "Specification-Title" to "gregtechmod",
-                "Specification-Vendor" to "Su5eD",
-                "Specification-Version" to "1",
-                "Implementation-Title" to project.name,
-                "Implementation-Version" to archiveVersion.get(),
-                "Implementation-Vendor" to "Su5eD",
-                "Implementation-Timestamp" to LocalDateTime.now()
-            )
-        }
+        manifest.attributes(manifestAttributes)
     }
 
     shadowJar {
-        dependsOn("classes", "jar")
+        dependsOn("jar")
         finalizedBy("reobfJar")
-
-        configurations = listOf(project.configurations["shade"])
-        from(sourceSets.getByName("api").output)
+        
+        configurations = listOf(shade)
+        manifest.attributes(manifestAttributes)
+        
+        from(api.output)
         relocate("com.fasterxml", "mods.gregtechmod.repack.fasterxml")
         relocate("org.yaml", "mods.gregtechmod.repack.yaml")
 
@@ -127,25 +128,14 @@ tasks {
 
     register<ShadowJar>("devJar") {
         dependsOn("classes")
-        configurations = listOf(project.configurations["shade"])
-
-        manifest {
-            attributes(
-                "Specification-Title" to "gregtechmod",
-                "Specification-Vendor" to "Su5eD",
-                "Specification-Version" to "1",
-                "Implementation-Title" to project.name,
-                "Implementation-Version" to archiveVersion.get(),
-                "Implementation-Vendor" to "Su5eD",
-                "Implementation-Timestamp" to LocalDateTime.now()
-            )
-        }
+        
+        configurations = listOf(shade)
+        manifest.attributes(manifestAttributes)
 
         relocate("com.fasterxml", "mods.gregtechmod.repack.fasterxml")
         relocate("org.yaml", "mods.gregtechmod.repack.yaml")
 
         from(sourceSets.main.get().output)
-        val api = sourceSets.getByName("api")
         from(api.output.classesDirs)
         from(api.output.resourcesDir)
 
@@ -155,14 +145,13 @@ tasks {
     register<Jar>("apiJar") {
         finalizedBy("reobfApiJar")
 
-        val api = sourceSets.getByName("api")
         from(api.allSource, api.output.classesDirs)
         exclude("META-INF/**")
         archiveClassifier.set("api")
     }
 
     register<Jar>("sourceJar") {
-        from(sourceSets.getByName("api").allSource)
+        from(api.allSource)
     }
 
     processResources {
@@ -171,7 +160,6 @@ tasks {
             "mcversion" to versionMc
         )
 
-        // replace stuff in mcmod.info, nothing else
         filesMatching("mcmod.info") {
             expand(
                 "version" to project.version,
@@ -192,7 +180,7 @@ repositories {
     mavenCentral()
     maven {
         name = "IC2"
-        url = uri("https://maven.ic2.player.to/")
+        url = uri("https://maven.ic2.player.to")
     }
     maven {
         name = "BuildCraft"
@@ -204,7 +192,7 @@ repositories {
     }
     maven {
         name = "Progwml6 maven"
-        url = uri("https://dvs1.progwml6.com/files/maven/")
+        url = uri("https://dvs1.progwml6.com/files/maven")
     }
     maven {
         name = "CoFH Maven"
@@ -215,17 +203,17 @@ repositories {
 dependencies {
     minecraft(group = "net.minecraftforge", name = "forge", version = "1.12.2-14.23.5.2855")
     
-    implementation(fg.deobf(group = "net.industrial-craft", name = "industrialcraft-2", version = "$versionIC2-ex112"))
-    api(fg.deobf(group = "net.industrial-craft", name = "industrialcraft-2", version = "$versionIC2-ex112", classifier = "api")) //GTE api depends on the ic2 api
+    implementation(fg.deobf(group = "net.industrial-craft", name = "industrialcraft-2", version = versionIC2))
+    api(fg.deobf(group = "net.industrial-craft", name = "industrialcraft-2", version = versionIC2, classifier = "api")) //GTE api depends on the ic2 api
     
-    compileOnly(fg.deobf(group = "cofh", name = "RedstoneFlux", version = "1.12-$versionRF", classifier = "universal"))
-    compileOnly(fg.deobf(group = "cofh", name = "CoFHCore", version = "1.12.2-$versionCoFHCore", classifier = "universal")) {
+    compileOnly(fg.deobf(group = "cofh", name = "RedstoneFlux", version = versionRF, classifier = "universal"))
+    compileOnly(fg.deobf(group = "cofh", name = "CoFHCore", version = versionCoFHCore, classifier = "universal")) {
         exclude(group = "mezz.jei")
     }
-    compileOnly(fg.deobf(group = "cofh", name = "CoFHWorld", version = "1.12.2-$versionCoFHWorld", classifier = "universal"))
-    compileOnly(fg.deobf(group = "cofh", name = "ThermalFoundation", version = "1.12.2-$versionThermalFoundation", classifier = "universal"))
-    implementation(fg.deobf(group = "codechicken", name = "CodeChickenLib", version = "1.12.2-$versionCodeChickenLib", classifier = "universal"))
-    compileOnly(fg.deobf(group = "cofh", name = "ThermalExpansion", version = "1.12.2-$versionThermalExpansion", classifier = "universal")) { 
+    compileOnly(fg.deobf(group = "cofh", name = "CoFHWorld", version = versionCoFHWorld, classifier = "universal"))
+    compileOnly(fg.deobf(group = "cofh", name = "ThermalFoundation", version = versionThermalFoundation, classifier = "universal"))
+    compileOnly(fg.deobf(group = "codechicken", name = "CodeChickenLib", version = versionCodeChickenLib, classifier = "universal"))
+    compileOnly(fg.deobf(group = "cofh", name = "ThermalExpansion", version = versionThermalExpansion, classifier = "universal")) { 
         exclude(group = "mezz.jei")
     }
     runtimeOnly(fg.deobf(group = "mezz.jei", name = "jei_$versionMc", version = versionJEI))
@@ -237,14 +225,17 @@ dependencies {
     compileOnly(fg.deobf(curse(mod = "thaumcraft", projectId = 223628, fileId = versionThaumcraft.toLong())))
     compileOnly(fg.deobf(group = "slimeknights.mantle", name = "Mantle", version = versionMantle))
     compileOnly(fg.deobf(group = "slimeknights", name = "TConstruct", version = versionTConstruct))
+    compileOnly(fg.deobf(curse(mod = "spark", projectId = 361579, fileId = 3245793)))
 
-    val databind = "com.fasterxml.jackson.core:jackson-databind:2.9.0"
-    implementation(databind)
-    "shade"(databind)
+    shade(group = "com.fasterxml.jackson.core", name = "jackson-databind", version = "2.9.0")
+    shade(group = "com.fasterxml.jackson.dataformat", name = "jackson-dataformat-yaml", version = "2.9.0")
+}
 
-    val yaml = "com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.9.0"
-    implementation(yaml)
-    "shade"(yaml)
+val projectArtifacts = sequenceOf("shadowJar", "apiJar", "devJar")
+    .map(tasks::getByName)
+
+artifacts { 
+    projectArtifacts.forEach(::archives)
 }
 
 publishing {
@@ -253,12 +244,11 @@ publishing {
             groupId = project.group as String
             artifactId = project.findProperty("archivesBaseName") as String
             version = project.version as String
-
-            artifact(tasks.getByName("shadowJar"))
-            artifact(tasks.getByName("apiJar"))
-            artifact(tasks.getByName("devJar"))
+            
+            projectArtifacts.forEach(::artifact)
         }
     }
+    
     repositories {
         maven {
             credentials {
