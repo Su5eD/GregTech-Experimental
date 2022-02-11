aa69aedddb0048fa18c8f662d39a9e60bb0c7aa4
Su5eD <su5ed@outlook.com>
2022-02-11 20:15:27 +0100

    Faster ConnectedModel updates

--- src/main/java/dev/su5ed/gregtechmod/block/ConnectedBlock.java
+++ src/main/java/dev/su5ed/gregtechmod/block/ConnectedBlock.java
@@ -1,61 +1,26 @@
 package dev.su5ed.gregtechmod.block;
 
 import dev.su5ed.gregtechmod.GregTechConfig;
-import dev.su5ed.gregtechmod.model.DirectionsProperty;
-import dev.su5ed.gregtechmod.model.DirectionsProperty.DirectionsWrapper;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.Property;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraftforge.client.model.data.ModelProperty;
 import one.util.streamex.StreamEx;
 
-import javax.annotation.Nullable;
+import java.util.EnumSet;
+import java.util.Set;
 
 public class ConnectedBlock extends ResourceBlock {
-    public static final Property<DirectionsWrapper> DIRECTIONS = new DirectionsProperty("directions"); // Try not using a property?
+    public static final ModelProperty<Set<Direction>> DIRECTIONS = new ModelProperty<>();
 
     public ConnectedBlock(float strength, float resistance) {
         super(strength, resistance);
     }
-
-    @Override
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
-        pBuilder.add(DIRECTIONS);
-    }
-
-    @SuppressWarnings("deprecation")
-    @Override
-    public void neighborChanged(BlockState pState, Level pLevel, BlockPos pPos, Block pBlock, BlockPos pFromPos, boolean pIsMoving) {
-        super.neighborChanged(pState, pLevel, pPos, pBlock, pFromPos, pIsMoving);
-        if (pState.getBlock() == this) pLevel.setBlockAndUpdate(pPos, getActualState(pLevel, pPos));
-    }
-
-    @Nullable
-    @Override
-    public BlockState getStateForPlacement(BlockPlaceContext pContext) {
-        Level world = pContext.getLevel();
-        BlockPos pos = pContext.getClickedPos();
-        
-        return getActualState(world, pos);
-    }
-
-    public BlockState getActualState(Level world, BlockPos pos) {
-        Direction[] dirs = StreamEx.of(Direction.values())
-            .filter(side -> isSideConnectable(world, pos, side))
-            .toArray(Direction[]::new);
-        return defaultBlockState()
-            .setValue(DIRECTIONS, DirectionsWrapper.from(dirs));
-    }
-
-    protected boolean isSideConnectable(Level world, BlockPos pos, Direction side) {
-        if (GregTechConfig.CLIENT.connectedTextures.get()) {
-            BlockState state = world.getBlockState(pos.relative(side));
-            return state.getBlock() == this;
-        }
-        return false;
+    
+    public Set<Direction> getConnections(BlockGetter world, BlockPos pos) {
+        return GregTechConfig.CLIENT.connectedTextures.get() ? StreamEx.of(Direction.values())
+            .filter(side -> world.getBlockState(pos.relative(side)).getBlock() == this)
+            .toCollection(() -> EnumSet.noneOf(Direction.class))
+            : Set.of();
     }
 }
--- src/main/java/dev/su5ed/gregtechmod/model/ConnectedModel.java
+++ src/main/java/dev/su5ed/gregtechmod/model/ConnectedModel.java
@@ -2,17 +2,20 @@ package dev.su5ed.gregtechmod.model;
 
 import com.mojang.math.Vector3f;
 import dev.su5ed.gregtechmod.block.ConnectedBlock;
-import dev.su5ed.gregtechmod.model.DirectionsProperty.DirectionsWrapper;
 import dev.su5ed.gregtechmod.util.GtUtil;
 import net.minecraft.client.renderer.block.model.*;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.resources.model.BlockModelRotation;
 import net.minecraft.client.resources.model.Material;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.client.model.data.IDynamicBakedModel;
 import net.minecraftforge.client.model.data.IModelData;
+import net.minecraftforge.client.model.data.ModelDataMap;
+import one.util.streamex.IntStreamEx;
 import one.util.streamex.StreamEx;
 
 import javax.annotation.Nonnull;
@@ -25,7 +28,7 @@ public class ConnectedModel implements IDynamicBakedModel {
     private static final BlockFaceUV FACE_UV = new BlockFaceUV(new float[] { 0, 0, 16, 16 }, 0);
     private static final Vector3f MAX = new Vector3f(16, 16, 16);
     private static final Vector3f MAX_DOWN = new Vector3f(16, 0, 16);
-    
+
     public static final List<String> TEXTURE_PARTS = Arrays.asList(
         "en", "ens", "ensw", "enw", "es",
         "esw", "ew", "ns", "nsw", "nw", "sw"
@@ -40,7 +43,7 @@ public class ConnectedModel implements IDynamicBakedModel {
     private final TextureAtlasSprite particle;
     private final ItemOverrides overrides;
     private final ItemTransforms transforms;
-    private final Map<DirectionsWrapper, Map<Direction, List<BakedQuad>>> quads;
+    private final Map<Set<Direction>, Map<Direction, List<BakedQuad>>> quads;
 
     public ConnectedModel(TextureAtlasSprite particle, Map<String, Material> materials, Function<Material, TextureAtlasSprite> spriteGetter, ItemOverrides overrides, ItemTransforms transforms, ResourceLocation modelLocation) {
         this.particle = particle;
@@ -49,21 +52,25 @@ public class ConnectedModel implements IDynamicBakedModel {
         this.quads = pregenQuads(materials, spriteGetter, modelLocation);
     }
 
-    private Map<DirectionsWrapper, Map<Direction, List<BakedQuad>>> pregenQuads(Map<String, Material> materials, Function<Material, TextureAtlasSprite> spriteGetter, ResourceLocation modelLocation) {
+    private Map<Set<Direction>, Map<Direction, List<BakedQuad>>> pregenQuads(Map<String, Material> materials, Function<Material, TextureAtlasSprite> spriteGetter, ResourceLocation modelLocation) {
         Map<Material, TextureAtlasSprite> sprites = StreamEx.ofValues(materials)
             .mapToEntry(spriteGetter)
             .toImmutableMap();
 
-        return StreamEx.of(DirectionsWrapper.VALUES)
-            .mapToEntry(key -> getQuadsForKey(key, materials, sprites, modelLocation))
+        return IntStreamEx.rangeClosed(0, 0b111111)
+            .mapToObj(i -> IntStreamEx.range(Direction.values().length)
+                .filter(j -> (i & 1 << j) != 0)
+                .mapToObj(Direction::from3DDataValue)
+                .toImmutableSet())
+            .mapToEntry(key -> getQuadsForSides(key, materials, sprites, modelLocation))
             .toImmutableMap();
     }
 
-    private Map<Direction, List<BakedQuad>> getQuadsForKey(DirectionsWrapper key, Map<String, Material> materials, Map<Material, TextureAtlasSprite> sprites, ResourceLocation modelLocation) {
+    private Map<Direction, List<BakedQuad>> getQuadsForSides(Set<Direction> sides, Map<String, Material> materials, Map<Material, TextureAtlasSprite> sprites, ResourceLocation modelLocation) {
         return StreamEx.of(Direction.values())
             .toMap(facing -> {
                 Vector3f to = facing == Direction.DOWN ? MAX_DOWN : MAX;
-                String texture = getTexture(key, facing, materials);
+                String texture = getTexture(sides, facing, materials);
                 Material material = materials.get(texture);
                 TextureAtlasSprite sprite = sprites.get(material);
                 BlockElementFace face = new BlockElementFace(facing.getOpposite(), 0, material.texture().toString(), FACE_UV);
@@ -75,18 +82,29 @@ public class ConnectedModel implements IDynamicBakedModel {
 
     @Nonnull
     @Override
+    public IModelData getModelData(@Nonnull BlockAndTintGetter world, @Nonnull BlockPos pos, @Nonnull BlockState state, @Nonnull IModelData tileData) {
+        if (state.getBlock() instanceof ConnectedBlock block) {
+            return new ModelDataMap.Builder()
+                .withInitial(ConnectedBlock.DIRECTIONS, block.getConnections(world, pos))
+                .build();
+        }
+        return tileData;
+    }
+
+    @Nonnull
+    @Override
     public List<BakedQuad> getQuads(@Nullable BlockState state, @Nullable Direction side, @Nonnull Random rand, @Nonnull IModelData extraData) {
         if (side != null) {
-            DirectionsWrapper key = getDirections(state);
+            Set<Direction> key = getConnections(extraData);
             Map<Direction, List<BakedQuad>> faceQuads = this.quads.get(key);
             return faceQuads.get(side);
         }
         return List.of();
     }
 
-    private String getTexture(DirectionsWrapper key, Direction side, Map<String, Material> materials) {
+    private String getTexture(Set<Direction> sides, Direction side, Map<String, Material> materials) {
         String connectionStringRaw = StreamEx.of(GtUtil.HORIZONTAL_FACINGS)
-            .filter(facing -> key.isSideConnected(getRelativeSide(side, facing)))
+            .filter(facing -> sides.contains(getRelativeSide(side, facing)))
             .map(facing -> {
                 Direction actualFacing = side == Direction.DOWN && facing.getAxis() == Direction.Axis.Z || GtUtil.isHorizontalFacing(side) && facing.getAxis() == Direction.Axis.X
                     ? facing.getOpposite()
@@ -151,7 +169,8 @@ public class ConnectedModel implements IDynamicBakedModel {
         return this.transforms;
     }
 
-    private static DirectionsProperty.DirectionsWrapper getDirections(@Nullable BlockState state) {
-        return state != null ? state.getValue(ConnectedBlock.DIRECTIONS) : DirectionsProperty.DirectionsWrapper.from();
+    private static Set<Direction> getConnections(IModelData data) {
+        Set<Direction> directions = data.getData(ConnectedBlock.DIRECTIONS);
+        return directions != null ? directions : Set.of();
     }
 }
